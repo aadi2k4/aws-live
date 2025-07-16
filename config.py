52d4467a6071f23e8c1bd8dab25237a94fc09018
AADITYA MANGAL <aaditya08r@gmail.com>
2025-07-16 11:40:26 +0530

    saved code

---
+++ config.py
@@ -0,0 +1,13 @@
+# Configuration Parameters for Database and AWS S3 Storage
+
+# Database Configuration
+customhost = "employee.cw3ee6a42ti0.us-east-1.rds.amazonaws.com"
+customuser = "aaditya"
+custompass = "Aaditya17"
+customdb = "employee"
+
+# AWS S3 Storage Configuration
+custombucket = "aadidatabase"
+customregion = "us-east-1"
+
+
